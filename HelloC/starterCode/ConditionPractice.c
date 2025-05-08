7b128960e8113b17f8c494ae7a146f16f0ce381d
dahn0209 <dahn0209@gmail.com>
2025-05-08 17:09:48 -0400

    add

--- HelloC/starterCode/ConditionPractice.c
+++ HelloC/starterCode/ConditionPractice.c
@@ -8,11 +8,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void simpleConditionPractice(int lower, int upper){
+    //if statement
+    if(lower< upper){
+        printf("Lower is less than upper\n");
+    }else if(lower>upper){
+        printf("Lower is greater than upper\n");
+
+    }else{
+        printf("Lower is equal than upper\n");
+
+    }
+
+    // one line if 
+    if(lower<upper) printf("Lower is less than upper\n");
+    else printf("Lower is not less than upper\n");
+
+
+}
+
+void complexCondition(int lower, int upper){
+    //if with logical operators
+    if(lower< upper && lower>0){// 0 <lower < upper <- not allowed
+        printf("Lower is less than upper and greater than 0\n");
+    }else if(lower>upper || lower<0){
+        printf("Lower is either greater than upper or less than 0\n");
+    }else{
+        printf("Lower is equal than upper and greater than or equal to 0\n");
+
+    }
+
+    //just using in values
+    if(lower-upper){
+        printf("lower is not equal to upper\n");
+    }else{
+        printf("lower is equal to upper\n");
+
+    }
+}
 
 
 
 
 int main() {
+    int lower=5;
+    int upper=10;
 
+    printf("Simple Condition Practice with (%d,%d)\n",lower,upper);
+    simpleConditionPractice(lower,upper);
+    complexCondition(lower,upper);
+
+    lower=10;
+    upper=10;
+
+    printf("Simple Condition Practice with (%d,%d)\n",lower,upper);
+    simpleConditionPractice(lower,upper);
+    complexCondition(lower,upper);
+
+
+    printf("\n");
     return 0;
 }
--- HelloC/starterCode/FunctionPractice.c
+++ HelloC/starterCode/FunctionPractice.c
@@ -9,9 +9,39 @@
 #include <stdlib.h>
 #include <stdint.h> 
 
+double add (double a,double b){
+    return a + b;
+}
+
+double subtract (double a,double b){
+    return a - b;
+}
+
+double multiply (double a,double b){
+    return a * b;
+}
+
+double divide (double a,double b){
+    return a / b;
+}
+
+double modulus (int64_t a,int64_t b){
+    return a % b;
+}
+
+
+
 
 int main() {
-    
+    long a=100;
+    short b=6;
+
+    printf("Addition: %.2f\n",add(a,b));
+    printf("Substraction: %.2f\n",subtract(a,b));
+    printf("Multiplication: %.2f\n",multiply(a,b));
+    printf("Division: %.2f\n",divide(a,b));
+    printf("Modulus: %.0f\n",modulus(a,b));
+
 
     return 0;
 }
