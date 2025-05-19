b51ffb4c7c2f59a51c3deab53c62e538fa9aff0a
dahn0209 <dahn0209@gmail.com>
2025-05-19 16:32:57 -0400

    worked on Swapper in Swapper.c

--- CandtheStack/starterCode/Swapper.c
+++ CandtheStack/starterCode/Swapper.c
@@ -7,12 +7,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+void  swap(int *a, int *b) {
+    int temp=*a;
+    *a=*b;
+    *b=temp;
+}
 
 int main() {
     int num1 = 10;
     int num2 = 20;
     printf("Values: num1 = %d, num2 = %d\n", num1, num2);
+    // ///Traditional way//
+    // int temp=num1;
+    // num1=num2;
+    // num2=temp;
+    swap(&num1, &num2);
     
     printf("Swapped values: num1 = %d, num2 = %d\n", num1, num2);
     return 0;
