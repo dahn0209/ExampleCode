2e792519de57b0814d9b5294b33c73367748e8bf
Albert Lionelle <lionelle@gmail.com>
2025-05-20 16:23:49 -0600

    cleaned comments

--- Graphs/solutionCode/GraphReader.h
+++ Graphs/solutionCode/GraphReader.h
@@ -17,15 +17,9 @@ typedef struct GraphReader {
     int* currentLine;
 } GraphReader;
 
-// Initialize a new GraphReader with the given filename
-// Returns NULL if the file cannot be opened
+
 GraphReader* reader_open(const char* filename);
-
-// Read the next line from the graph file
-// Returns an array of 3 integers or NULL if end of file or error
 int* reader_next(GraphReader* reader);
-
-// Close the GraphReader and free allocated memory
 void reader_close(GraphReader* reader);
 
 #endif /* GRAPH_READER_H */
