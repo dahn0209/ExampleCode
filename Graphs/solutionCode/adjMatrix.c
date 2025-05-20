b9fb2341b3b3774a1add1f301acdc5fdf4e80060
Albert Lionelle <lionelle@gmail.com>
2025-05-20 16:27:09 -0600

    initial file setup

---
+++ Graphs/solutionCode/adjMatrix.c
@@ -0,0 +1,14 @@
+/**
+* Solution file for Code Along Adjacency Matrix
+* 
+* @author Albert Lionelle
+* @date 2025-05-20
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "GraphReader.h"
+#include "adjMatrix.h"
+
---
+++ Graphs/solutionCode/adjMatrix.h
@@ -0,0 +1,15 @@
+/*
+ * adjMatrix.h
+ * 
+ * Description: Header file for adjacency matrix implementation of a graph
+**/
+
+#ifndef ADJ_MATRIX_H
+#define ADJ_MATRIX_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#endif // ADJ_MATRIX_H
