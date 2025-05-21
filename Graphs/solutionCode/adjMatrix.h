606f21d2fff80536f237f02f879068df8f35306b
Albert Lionelle <lionelle@gmail.com>
2025-05-20 20:02:36 -0600

    initial solution

--- Graphs/solutionCode/adjMatrix.h
+++ Graphs/solutionCode/adjMatrix.h
@@ -12,4 +12,26 @@
 #include <stdbool.h>
 
 
+
+typedef struct  {
+    int numVertices;
+    int capacity;
+    int* adjMatrix;
+    bool directed;
+} AdjMatrixGraph;
+
+#define SCALE_FACTOR 2
+#define DEFAULT_WEIGHTS 0
+
+AdjMatrixGraph* createGraph(int capacity, bool directed); 
+void freeGraph(AdjMatrixGraph* graph);
+
+void addEdge(AdjMatrixGraph* graph, int src, int dest, int weight);
+int getDegree(AdjMatrixGraph* graph, int vertex);
+int* getNeighbors(AdjMatrixGraph* graph, int vertex);
+int getWeight(AdjMatrixGraph* graph, int src, int dest);
+void printGraph(AdjMatrixGraph* graph);
+void loadFromFile(AdjMatrixGraph* graph, const char* filename);
+
+
 #endif // ADJ_MATRIX_H
